143d5058c666d0b79b24a576a7c48b742a6e45e6
prakul19 <prakulagarwal5@gmail.com>
2025-04-28 09:48:39 +0530

    [prakul] stats: Setting up the project

---
+++ src/main/java/com/bridgelabz/expense_splitter_debt_settler/ExpenseSplitterDebtSettlerApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.expense_splitter_debt_settler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseSplitterDebtSettlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseSplitterDebtSettlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridgelabz/expense_splitter_debt_settler/ExpenseSplitterDebtSettlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.expense_splitter_debt_settler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseSplitterDebtSettlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
